ea8a2d1b5fa836b37facc586eb38d6ff90361356
Kevin Griffin <griffin.kev@gmail.com>
2010-04-13 23:07:28 -0400

    initial commit

---
+++ src/com/chariotsolutions/mobile/MainTabs.java
@@ -0,0 +1,92 @@
+package com.chariotsolutions.mobile;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainTabs extends Activity {
+
+	private TextView mDateDisplay;
+	private Button mPickDate;
+
+	private int mYear;
+	private int mMonth;
+	private int mDay;
+
+	static final int DATE_DIALOG_ID = 0;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		/** Project Spinner */
+		Spinner s = (Spinner) findViewById(R.id.spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+				R.array.projects, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		s.setAdapter(adapter);
+
+		/** Date Picker */
+		// capture our View elements
+		mDateDisplay = (TextView) findViewById(R.id.dateDisplay);
+		mPickDate = (Button) findViewById(R.id.pickDate);
+
+		// add a click listener to the button
+		mPickDate.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				showDialog(DATE_DIALOG_ID);
+			}
+		});
+
+		// get the current date
+		final Calendar c = Calendar.getInstance();
+		mYear = c.get(Calendar.YEAR);
+		mMonth = c.get(Calendar.MONTH);
+		mDay = c.get(Calendar.DAY_OF_MONTH);
+
+		// display the current date
+		updateDisplay();
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case DATE_DIALOG_ID:
+			return new DatePickerDialog(this, mDateSetListener, mYear, mMonth, mDay);
+		}
+		return null;
+	}
+
+	// updates the date we display in the TextView
+	private void updateDisplay() {
+		mDateDisplay.setText(new StringBuilder()
+				// Month is 0 based so add 1
+				.append(mMonth + 1).append("-").append(mDay).append("-").append(mYear)
+				.append(" "));
+	}
+
+	// the callback received when the user "sets" the date in the dialog
+	private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			mYear = year;
+			mMonth = monthOfYear;
+			mDay = dayOfMonth;
+			updateDisplay();
+		}
+	};
+
+}
